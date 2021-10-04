fd598ce459888a72adc6680d433c066d7be3afbf
kim.mcintyre <kimberly.mcintyre.main@gmail.com>
2021-10-04 15:10:37 -0400

    Replaced appflow_build.sh with js equivalent

---
+++ appflow_build.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+if (process.env.CI_APP_ID == "b57b6ac4") {
+    console.log("this is appflow");
+}
--- appflow_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ $CI_APP_ID == "b57b6ac4" ]; then
-    #If that check is true we know the build is running in appflow
-    echo "this is appflow"
-fi
